12262b1a734b952545052d667dd0cc48f60eb2f8
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-10-03 11:19:28 +0200

    ES2015ify tests

--- test.js
+++ test.js
@@ -1,21 +1,20 @@
-'use strict';
-var test = require('ava');
-var lastLeapYear = require('./');
+import test from 'ava';
+import lastLeapYear from './';
 
-test('should return the last leap year', function (t) {
+test('should return the last leap year', t => {
 	t.is(lastLeapYear(new Date(-61206064203841)), 28);
 	t.is(lastLeapYear(new Date('January 10, 2008')), 2008);
 
 	t.end();
 });
 
-test('should handle passing a year', function (t) {
+test('should handle passing a year', t => {
 	t.is(lastLeapYear(2015), 2012);
 
 	t.end();
 });
 
-test('should default to taking the current timestamp', function (t) {
+test('should default to taking the current timestamp', t => {
 	t.is(lastLeapYear(), 2012);
 
 	t.end();
