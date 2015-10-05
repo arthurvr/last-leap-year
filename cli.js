0a94410ae9e43f18aafb6f9de4c74000f1cb6953
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-10-05 13:12:51 +0200

    Simplify CLI help creation

--- cli.js
+++ cli.js
@@ -3,19 +3,17 @@
 var meow = require('meow');
 var lastLeapYear = require('./');
 
-var cli = meow({
-	help: [
-		'Usage',
-		'  $ last-leap-year [input]',
-		'',
-		'Examples',
-		'  $ last-leap-year',
-		'  2012',
-		'',
-		'  $ last-leap-year 2010',
-		'  2008'
-	]
-});
+var cli = meow([
+	'Usage',
+	'  $ last-leap-year [input]',
+	'',
+	'Examples',
+	'  $ last-leap-year',
+	'  2012',
+	'',
+	'  $ last-leap-year 2010',
+	'  2008'
+]);
 
 var year = cli.input ? Number(cli.input) : (new Date()).getFullYear();
 console.log(lastLeapYear(year));
